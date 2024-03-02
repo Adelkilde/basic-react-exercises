9a2bd56d81de2e63ed411fb8955fa828e43f5e42
Adelkilde <William.adelkilde@gmail.com>
2024-03-02 12:17:50 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://adelkilde.github.io/day1example-v2/",
+  base: "https://adelkilde.github.io/basic-react-exercises/",
 });
